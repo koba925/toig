00aeecf669be25521cdfba97c5307ee596a42cba
Kobayashi Takahiro <koba925@gmail.com>
2025-03-29 12:24:27 +0000

    エラー処理を追加する

--- toig.py
+++ toig.py
@@ -1,10 +1,17 @@
 # environment
 
 def define(env, name, val):
+    assert name not in env["vals"], \
+        f"Variable already defined: `{name}` @ define"
     env["vals"][name] = val
+    return val
 
 def get(env, name):
-    return env["vals"][name] if name in env["vals"] else get(env["parent"], name)
+    assert env is not None, f"Undefined variable: `{name}` @ get"
+    if name in env["vals"]:
+        return env["vals"][name]
+    else:
+        return get(env["parent"], name)
 
 # evaluator
 
@@ -19,19 +26,30 @@ def eval(expr, env):
             return eval(thn, env) if eval(cnd, env) else eval(els, env)
         case [func, *args]:
             return apply(eval(func, env), [eval(arg, env) for arg in args])
+        case unexpected:
+            assert False, f"Unexpected expression: {unexpected} @ eval"
 
 def apply(f_val, args_val):
     if callable(f_val): return f_val(args_val)
     _, params, body, env = f_val
+    assert len(params) == len(args_val), \
+        f"Argument count doesn't match: `{args_val}` @ apply"
     env = {"parent": env, "vals": dict(zip(params, args_val))}
     return eval(body, env)
 
 # runtime
 
+import operator as op
+
+def binary_op(f, args):
+    assert len(args) == 2, \
+        f"Argument count doesn't match: `{args}` @ binary_op"
+    return f(args[0], args[1])
+
 builtins = {
-    "+": lambda args: args[0] + args[1],
-    "-": lambda args: args[0] - args[1],
-    "=": lambda args: args[0] == args[1],
+    "+": lambda args_val: binary_op(op.add, args_val),
+    "-": lambda args_val: binary_op(op.sub, args_val),
+    "=": lambda args_val: binary_op(op.eq, args_val),
 }
 top_env = {"parent": None, "vals": builtins}
 
@@ -40,23 +58,35 @@ def run(src):
 
 # tests
 
+def fails(expr):
+    try: run(expr)
+    except AssertionError: return True
+    else: return False
+
 assert run(None) == None
 assert run(5) == 5
 assert run(True) == True
 assert run(False) == False
 
-run(["define", "a", 5])
+assert run(["define", "a", 5]) == 5
 assert run("a") == 5
+assert fails(["define", "a", 6])
+assert fails(["b"])
 
 assert run(["if", True, 5, 6]) == 5
 assert run(["if", False, 5, 6]) == 6
+assert fails(["if", True, 5])
 
 assert run(["+", 5, 6]) == 11
 assert run(["-", 11, 5]) == 6
 assert run(["=", 5, 5]) == True
 assert run(["=", 5, 6]) == False
+assert fails(["+", 5])
+assert fails(["+", 5, 6, 7])
 
 assert run([["func", ["n"], ["+", 5, "n"]], 6]) == 11
+assert fails([["func", ["n"], ["+", 5, "n"]]])
+assert fails([["func", ["n"], ["+", 5, "n"]], 6, 7])
 
 run(["define", "fib", ["func", ["n"],
         ["if", ["=", "n", 0], 0,
