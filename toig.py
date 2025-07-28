b6546a209ba0f74d05dd7ab1a36cd4011e30ffc1
Kobayashi Takahiro <koba925@gmail.com>
2025-07-28 23:14:40 +0000

    envを引数で渡す

--- toig.py
+++ toig.py
@@ -31,60 +31,55 @@ class Environment:
         return env
 
 class Evaluator:
-    def __init__(self, env):
-        self._env = env
-
-    def eval(self, expr):
+    def eval(self, expr, env):
         match expr:
             case None | bool(_) | int(_):
                 return expr
             case str(name):
-                return self._env.get(name)
+                return env.get(name)
             case ["func", params, body]:
-                return ["func", params, body, self._env]
+                return ["func", params, body, env]
             case ["define", name, val]:
-                return self._env.define(name, self.eval(val))
+                return env.define(name, self.eval(val, env))
             case ["assign", name, val]:
-                return self._env.assign(name, self.eval(val))
+                return env.assign(name, self.eval(val, env))
             case ["seq", *exprs]:
-                return self._eval_seq(exprs)
+                return self._eval_seq(exprs, env)
             case ["if", cnd, thn, els]:
-                return self._eval_if(cnd, thn, els)
+                return self._eval_if(cnd, thn, els, env)
             case [func, *args]:
                 return self._apply(
-                    self.eval(func),
-                    [self.eval(arg) for arg in args])
+                    self.eval(func, env),
+                    [self.eval(arg, env) for arg in args])
             case unexpected:
                 assert False, f"Unexpected expression: {unexpected} @ eval"
 
-    def _eval_seq(self, exprs):
+    def _eval_seq(self, exprs, env):
         val = None
         for expr in exprs:
-            val = self.eval(expr)
+            val = self.eval(expr, env)
         return val
 
-    def _eval_if(self, cnd, thn, els):
-        if self.eval(cnd):
-            return self.eval(thn)
+    def _eval_if(self, cnd, thn, els, env):
+        if self.eval(cnd, env):
+            return self.eval(thn, env)
         else:
-            return self.eval(els)
+            return self.eval(els, env)
 
     def _apply(self, f_val, args_val):
         if callable(f_val):
             return f_val(*args_val)
         _, params, body, env = f_val
-        prev_env = self._env
-        self._env =  env.extend(params, args_val)
-        val = self.eval(body)
-        self._env = prev_env
-        return val
+        return self.eval(body, env.extend(params, args_val))
 
 import operator
 
 class Interpreter:
     def __init__(self):
         self._env = Environment()
+        self.init_builtins()
 
+    def init_builtins(self):
         _builtins = {
             "__builtins__": None,
             "add": operator.add,
@@ -99,7 +94,7 @@ class Interpreter:
         self._env = Environment(self._env)
 
     def go(self, src):
-        return Evaluator(self._env).eval(src)
+        return Evaluator().eval(src, self._env)
 
 if __name__ == "__main__":
     i = Interpreter()
