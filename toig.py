e60c0f113c18d0b7b2d4ecd3847eb6f496990e1a
Kobayashi Takahiro <koba925@gmail.com>
2025-08-02 07:42:40 +0000

    エラー時に行番号を表示する

--- toig.py
+++ toig.py
@@ -3,39 +3,65 @@
 def is_name_first(c): return c.isalpha() or c == "_"
 def is_name_rest(c): return c.isalnum() or c == "_"
 
+import dataclasses
+
+@dataclasses.dataclass(frozen=True)
+class Token:
+    val: None | bool | int | str | list
+    text: str
+    line: int
+
+    def with_val(self, val):
+        return dataclasses.replace(self, val=val)
+
+from typing import NoReturn
+
+def report_error(msg, text, line) -> NoReturn:
+    if text == "$EOF":
+        raise AssertionError(msg + f" at end")
+    else:
+        raise AssertionError(msg + f": `{text}` at line {line}")
+
 class Scanner():
     def __init__(self, src):
         self._src = src
         self._pos = 0
-        self._token = ""
+        self._line = 1
+        self._text = ""
+
+    def __repr__(self):
+        return f"Scanner(_pos={self._pos}, _line={self._line}, _text='{self._text}')"
 
     def next_token(self):
         while self._current_char().isspace(): self._advance()
 
-        self._token = ""
+        self._text = ""
 
         match self._current_char():
-            case "$EOF": return "$EOF"
+            case "$EOF":
+                return Token("$EOF", "$EOF", self._line)
             case c if is_name_first(c):
                 return self._name()
             case c if c.isnumeric():
                 self._word(str.isnumeric)
-                return int(self._token)
+                return self._token(int(self._text))
             case c if c in "=:":
                 self._append_char()
                 if self._current_char() == "=": self._append_char()
+                return self._token(self._text)
             case c if c in "+-(),;":
                 self._append_char()
-
-        return self._token
+                return self._token(self._text)
+            case c:
+                report_error("Unexpected character", c, self._line)
 
     def _name(self):
         self._word(is_name_rest)
-        match self._token:
-            case "None": return None
-            case "True": return True
-            case "False": return False
-            case _ : return self._token
+        match self._text:
+            case "None": return self._token(None)
+            case "True": return self._token(True)
+            case "False": return self._token(False)
+            case text : return self._token(text)
 
     def _word(self, is_rest):
         self._append_char()
@@ -43,7 +69,7 @@ class Scanner():
             self._append_char()
 
     def _append_char(self):
-        self._token += self._current_char()
+        self._text += self._current_char()
         self._advance()
 
     def _current_char(self):
@@ -52,8 +78,13 @@ class Scanner():
         else:
             return "$EOF"
 
-    def _advance(self): self._pos += 1
+    def _advance(self):
+        if self._current_char() == "\n":
+            self._line += 1
+        self._pos += 1
 
+    def _token(self, val):
+        return Token(val, self._text, self._line)
 
 # Parser
 
@@ -63,10 +94,13 @@ class Parser:
         self._scanner = Scanner(src)
         self._current_token = self._scanner.next_token()
 
+    def __repr__(self):
+        return f"Parser(_current_token={self._current_token})"
+
     def parse(self):
         expr = self._expression()
-        assert self._current_token == "$EOF", \
-            f"Unexpected token at end: `{self._current_token}` @ parse"
+        if self._current_token.val != "$EOF":
+            self._report_error("Unexpected token at end")
         return expr
 
     # Grammar
@@ -75,11 +109,16 @@ class Parser:
         return self._sequence()
 
     def _sequence(self):
-        exprs = [self._define_assign()]
-        while self._current_token == ";":
-            self._advance()
-            exprs.append(self._define_assign())
-        return exprs[0] if len(exprs) == 1 else ["seq"] + exprs
+        expr = self._define_assign()
+        if self._current_token.val != ";":
+            return expr
+        else:
+            op = self._current_token.with_val("seq")
+            seq = [op, expr]
+            while self._current_token.val == ";":
+                self._advance()
+                seq.append(self._define_assign())
+            return seq
 
     def _define_assign(self):
         return self._binary_right({
@@ -98,83 +137,88 @@ class Parser:
 
     def _call(self):
         target = self._primary()
-        while self._current_token == "(":
+        while self._current_token.val == "(":
             self._advance()
             target = [target] + self._comma_separated_exprs(")")
         return target
 
     def _primary(self):
-        match self._current_token:
+        match self._current_token.val:
             case "(":
                 self._advance()
                 expr = self._expression()
                 self._consume(")")
                 return expr
             case "func":
-                self._advance(); return self._func()
+                return self._func()
             case "if":
-                self._advance(); return self._if()
+                return self._if()
         return self._advance()
 
     def _if(self):
+        op = self._advance().with_val("if")
         cnd = self._expression()
         self._consume("then")
         thn = self._expression()
-        if self._current_token == "elif":
-            self._advance()
-            return ["if", cnd, thn, self._if()]
-        if self._current_token == "else":
+        if self._current_token.val == "elif":
+            return [op, cnd, thn, self._if()]
+        if self._current_token.val == "else":
             self._advance()
             els = self._expression()
             self._consume("end")
-            return ["if", cnd, thn, els]
+            return [op, cnd, thn, els]
         self._consume("end")
-        return ["if", cnd, thn, None]
+        return [op, cnd, thn, Token(None, "", op.line)]
 
     def _func(self):
+        op = self._advance()
         self._consume("(")
         params = self._comma_separated_exprs(")")
         self._consume("do")
         body = self._expression()
         self._consume("end")
-        return ["func", params, body]
+        return [op, params, body]
 
     # Helpers
 
     def _binary_left(self, ops, sub_elem):
         left = sub_elem()
-        while (op := self._current_token) in ops:
+        while (op := self._current_token).text in ops:
             self._advance()
-            left = [ops[op], left, sub_elem()]
+            left = [op.with_val(ops[op.text]), left, sub_elem()]
         return left
 
     def _binary_right(self, ops, sub_elem):
         left = sub_elem()
-        if (op := self._current_token) in ops:
+        if (op := self._current_token).text in ops:
             self._advance()
-            return [ops[op], left, self._binary_right(ops, sub_elem)]
+            return [op.with_val(ops[op.text]),
+                left, self._binary_right(ops, sub_elem)]
         return left
 
     def _comma_separated_exprs(self, closing_token):
         cse = []
-        if self._current_token != closing_token:
+        if self._current_token.val != closing_token:
             cse.append(self._expression())
-            while self._current_token == ",":
+            while self._current_token.val == ",":
                 self._advance()
                 cse.append(self._expression())
         self._consume(closing_token)
         return cse
 
     def _consume(self, expected):
-        assert isinstance(self._current_token, str) and \
-            self._current_token in expected, \
-            f"Expected `{expected}`, found `{self._current_token}` @ consume"
+        if not isinstance(self._current_token.val, str) or \
+                self._current_token.val not in expected:
+            self._report_error(f"`{expected}` expected")
         return self._advance()
 
     def _advance(self):
         prev_token = self._current_token
         self._current_token = self._scanner.next_token()
         return prev_token
+
+    def _report_error(self, msg):
+        report_error(msg, self._current_token.text, self._current_token.line)
 
 # Evaluator
 
@@ -207,26 +251,24 @@ class Environment:
 class Evaluator:
     def eval(self, expr, env):
         match expr:
-            case None | bool(_) | int(_):
-                return expr
-            case str(name):
-                return env.get(name)
-            case ["func", params, body]:
+            case Token(val=v):
+                return env.get(v) if isinstance(v, str) else v
+            case [Token(val="func"), params, body]:
                 return ["func", params, body, env]
-            case ["define", name, val]:
-                return env.define(name, self.eval(val, env))
-            case ["assign", name, val]:
-                return env.assign(name, self.eval(val, env))
-            case ["seq", *exprs]:
+            case [Token(val="define"), name, val]:
+                return env.define(name.val, self.eval(val, env))
+            case [Token(val="assign"), name, val]:
+                return env.assign(name.val, self.eval(val, env))
+            case [Token(val="seq"), *exprs]:
                 return self._eval_seq(exprs, env)
-            case ["if", cnd, thn, els]:
+            case [Token(val="if"), cnd, thn, els]:
                 return self._eval_if(cnd, thn, els, env)
             case [func, *args]:
                 return self._apply(
                     self.eval(func, env),
                     [self.eval(arg, env) for arg in args])
             case unexpected:
-                assert False, f"Unexpected expression: {unexpected} @ eval"
+                report_error("Unexpected expression", unexpected, 0)
 
     def _eval_seq(self, exprs, env):
         val = None
@@ -247,7 +289,7 @@ class Evaluator:
         _, params, body, env = f_val
         new_env = Environment(env)
         for param, arg in zip(params, args_val):
-            new_env.define(param, arg)
+            new_env.define(param.val, arg)
         return self.eval(body, new_env)
 
 class Interpreter:
